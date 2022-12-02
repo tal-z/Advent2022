01ec1a948118ba991fe48dd64ccbcc6391d6d257
tal-z <tal.zaken@pearlhealth.com>
2022-12-02 11:13:19 -0500

    Day 2 solutions

---
+++ solutions/day_2_solution_1.py
@@ -0,0 +1,43 @@
+from utils import read_input
+
+
+move_values = {
+    # Player 1
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    # Player 2
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+
+def score_round(p1_value, p2_value):
+    """
+    The score for a single round is the score for the shape you selected
+    (1 for Rock, 2 for Paper, and 3 for Scissors)
+    plus the score for the outcome of the round
+    (0 if you lost, 3 if the round was a draw, and 6 if you won).
+    """
+    outcome = p2_value-p1_value
+    if outcome in {1, -2}:
+        return p2_value + 6
+    if not outcome:
+        return p2_value + 3
+    return p2_value
+
+if __name__ == "__main__":
+
+    data = read_input(2)
+
+    total_score = 0
+    for moves in data:
+        p1_move, p2_move = moves.strip().split()
+
+        p1_value = move_values[p1_move]
+        p2_value = move_values[p2_move]
+
+        total_score += score_round(p1_value, p2_value)
+
+    print(total_score)
---
+++ solutions/day_2_solution_2.py
@@ -0,0 +1,53 @@
+from utils import read_input
+from day_2_solution_1 import score_round
+
+move_values = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+}
+
+win_moves = {
+    "A": "B",
+    "B": "C",
+    "C": "A",
+}
+
+draw_moves = {
+    "A": "A",
+    "B": "B",
+    "C": "C",
+}
+
+lose_moves = {
+    "A": "C",
+    "B": "A",
+    "C": "B",
+}
+
+needed_moves = {
+    "X": lose_moves,
+    "Y": draw_moves,
+    "Z": win_moves,
+}
+
+
+def needed_move(p1_move, win_lose_draw_val):
+    return needed_moves[win_lose_draw_val][p1_move]
+
+
+if __name__ == "__main__":
+
+    data = read_input(2)
+
+    total_score = 0
+    for moves in data:
+        p1_move, win_lose_draw_val = moves.strip().split()
+        p2_move = needed_move(p1_move, win_lose_draw_val)
+
+        p1_value = move_values[p1_move]
+        p2_value = move_values[p2_move]
+
+        total_score += score_round(p1_value, p2_value)
+
+    print(total_score)
